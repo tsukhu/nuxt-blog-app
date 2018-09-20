d30183f317a8b0990a9c27f3c27e7345d9fd1392
Tarun Kumar Sukhu <tksukhu@gmail.com>
2018-09-20 13:10:24 +0530

    Completed the basic layout

---
+++ components/Posts/PostList.vue
@@ -0,0 +1,48 @@
+<template>
+    <section class="post-list">
+      <PostPreview 
+        id="1"
+        :is-admin="isAdmin" 
+        thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
+        title="Hello there"
+        previewText="This is my first post"/>
+      <PostPreview 
+        id="2"
+        :is-admin="isAdmin" 
+        thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
+        title="Hello there one more"
+        previewText="This is my first post"/>
+      <PostPreview 
+        id="3"
+        :is-admin="isAdmin"  
+        thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
+        title="Hello there more"
+        previewText="This is my new post!"/>
+    </section>
+</template>
+
+<script>
+import PostPreview from '@/components/Posts/PostPreview';
+export default {
+  components: {
+    PostPreview
+  },
+  props: {
+    isAdmin: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style scoped>
+.post-list {
+  display: flex;
+  padding: 20px;
+  box-sizing: border-box;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -3,32 +3,16 @@
     <section class="intro">
       <h1>Get the latest tech news!</h1>
     </section>
-    <section class="featured-posts">
-      <PostPreview 
-      id="1" 
-      thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
-      title="Hello there"
-      previewText="This is my first post"/>
-      <PostPreview 
-      id="2" 
-      thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
-      title="Hello there one more"
-      previewText="This is my first post"/>
-            <PostPreview 
-      id="3" 
-      thumbnail="https://images.pexels.com/photos/96381/pexels-photo-96381.jpeg?cs=srgb&dl=abstract-art-blur-96381.jpg&fm=jpg"
-      title="Hello there more"
-      previewText="This is my new post!"/>
-    </section>
+    <PostList/>
   </div>
 </template>
 
 <script>
-import PostPreview from '@/components/Posts/PostPreview';
+import PostList from '@/components/Posts/PostList';
 
 export default {
   components: {
-    PostPreview
+    PostList
   }
 };
 </script>
@@ -63,14 +47,5 @@ export default {
   .intro h1 {
     font-size: 2rem;
   }
-}
-
-.featured-posts {
-  display: flex;
-  padding: 20px;
-  box-sizing: border-box;
-  flex-wrap: wrap;
-  align-items: center;
-  justify-content: center;
 }
 </style>
